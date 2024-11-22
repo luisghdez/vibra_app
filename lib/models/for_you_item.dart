b7d4b1497628a848bb57bf08016fe9fe19ceac00
luisghdez <1gluishernandez@gmail.com.com>
2024-11-21 19:28:47 -0700

    sm

---
+++ lib/models/for_you_item.dart
@@ -0,0 +1,19 @@
+class ForYouItem {
+  final String imageUrl;
+  final String title;
+  final String creator;
+  final int friendsCount;
+  final String date;
+  final String time;
+  final String location;
+
+  ForYouItem({
+    required this.imageUrl,
+    required this.title,
+    required this.creator,
+    required this.friendsCount,
+    required this.date,
+    required this.time,
+    required this.location,
+  });
+}
